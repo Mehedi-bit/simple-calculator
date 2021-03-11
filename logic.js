3b840a825b5fbd1021883e36ead44ef1a386c007
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2021-03-11 12:01:00 +0600

    yeagit add . simple calculator making done

--- logic.js
+++ logic.js
@@ -1,7 +1,28 @@
 let display = document.getElementById("display");
 let buttons = document.querySelectorAll('button');
-for (const item of buttons) {
-    item.addEventListener('click', (event){
-        buttonText = event.target.innerText;
-    }
+
+let displayValue = '';
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function(event){
+        let buttonText = event.target.innerText;
+        console.log(buttonText);
+        if (buttonText == 'x') {
+            buttonText = '*';
+            displayValue += buttonText;
+            display.value = displayValue;
+
+        }
+        else if(buttonText == 'AC'){
+            displayValue = '';
+            display.value = displayValue;
+        }
+        else if(buttonText == '='){
+            display.value = eval(displayValue);
+        }
+        else{
+            displayValue += buttonText;
+            display.value = displayValue;
+        }
+    })
 }
